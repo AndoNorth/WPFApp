c31f16081d2813ee401550e0e0cb51e332a681fa
Ando <55718737+AndoNorth@users.noreply.github.com>
2023-02-26 22:25:01 +0000

    Flesh out initial main view model
    1) add initial view model for discover tab to main app
    2) create templates for the search box
    3) create home view and enable on main app
    4) create resources for views and templates

---
+++ TestWPF/TestWPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestWPF.Core;
+namespace TestWPF.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanges();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
